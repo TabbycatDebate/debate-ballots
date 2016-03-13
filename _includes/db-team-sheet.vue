0843ce3b9b50fbafb3d126037fdedf2582174b56
Philip Belesky <contact@philipbelesky.com>
2016-03-13 16:27:05 +1100

    Layout changes, add local storage
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- _includes/db-team-sheet.vue
+++ _includes/db-team-sheet.vue
@@ -3,8 +3,8 @@
   <div class="db-flex-item-2 db-flex-row db-bottom-border">
     <div class="db-padding-horizontal db-flex-item db-align-vertical-center db-flex-static db-right-text">
       <template v-if="isBP === false">
-        <span v-if="n === 0">Affirmative Team:</span>
-        <span v-if="n === 1">Negative Team:</span>
+        <span v-if="n === 0">Affirmative:</span>
+        <span v-if="n === 1">Negative:</span>
       </template>
       <template v-if="isBP === true">
         <span v-if="n === 0">Opening Government:</span>
@@ -17,10 +17,10 @@
     </div>
   </div>
 
-  <div class="db-flex-item db-flex-row db-bottom-border db-flex-item-1 db-dont-fill-in"><!-- Keys -->
-    <div class="db-flex-item-fws">
+  <div class="db-flex-item-2 db-flex-row db-bottom-border  db-dont-fill-in"><!-- Keys -->
+    <div class="db-align-vertical-center  db-left-text db-padding-horizontal" v-bind:class="{ 'db-flex-item-fws': isBP === false, 'db-flex-item-fwm': isBP === true }">
     </div>
-    <div v-show="showPronouns" class="db-align-horizontal-center db-align-vertical-center db-padding-horizontal db-left-text" db-flex-item-fwl">
+    <div v-show="showPronouns" class="db-align-horizontal-center db-align-vertical-center db-left-text" db-flex-item-fwl">
       Pronoun
     </div>
     <div class="db-align-horizontal-center db-align-vertical-center db-padding-horizontal db-flex-item  db-center-text">
@@ -31,13 +31,29 @@
     </div>
   </div>
 
-  <div class="db-flex-item-2 db-flex-row db-bottom-border" v-for="n in speakersCount"><!-- Speakers -->
-    <div class="db-align-vertical-center db-align-horizontal-end db-right-text db-flex-item-fws">
-      [[ n + 1 ]].
+  <div class="db-flex-item-2 db-flex-row db-bottom-border" v-for="x in speakersCount"><!-- Speakers -->
+    <div class="db-align-vertical-center  db-left-text db-padding-horizontal" v-bind:class="{ 'db-flex-item-fws': isBP === false, 'db-flex-item-fwm': isBP === true }">
+      <template v-if="isBP === false">
+        [[ x + 1 ]].
+      </template>
+      <template v-if="isBP === true">
+        <span v-if="n === 0">
+          <span v-if="x === 0">PM.</span><span v-else>DPM.</span>
+        </span>
+        <span v-if="n === 1">
+          <span v-if="x === 0">LO.</span><span v-else>DLO.</span>
+        </span>
+        <span v-if="n === 2">
+          <span v-if="x === 0">GM.</span><span v-else>GW.</span>
+        </span>
+        <span v-if="n === 3">
+          <span v-if="x === 0">OM.</span><span v-else>OW.</span>
+        </span>
+      </template>
     </div>
-    <div v-show="showPronouns" class="db-padding-horizontal db-fill-in db-flex-item-fwl">
+    <div v-show="showPronouns" class="db-fill-in db-flex-item-fwl">
     </div>
-    <div class="db-padding-horizontal db-fill-in  db-flex-item">
+    <div class="db-padding-horizontal db-fill-in db-flex-item">
     </div>
     <div class="db-padding-horizontal db-fill-in db-flex-item-fwl">
     </div>
@@ -56,16 +72,24 @@
   </div>
 
   <div class="db-flex-item-2 db-flex-row"><!-- Totals -->
-    <div class="db-padding-horizontal db-align-vertical-center db-align-horizontal-end db-right-text db-flex-item-fws">
+    <div class="db-align-vertical-center  db-left-text db-padding-horizontal" v-bind:class="{ 'db-flex-item-fws': isBP === false, 'db-flex-item-fwm': isBP === true }">
     </div>
-    <div class="db-flex-item db-align-vertical-center">
-      <em>
-        Speaker scores range from [[ lowestSpeak ]] to [[ highestSpeak ]]
-        <span v-show="hasReplies"> and [[ lowestSpeak/2 ]] to [[ highestSpeak/2 ]] for reply scores.</span>
-      </em>
+    <div class="db-flex-item db-flex-row db-align-vertical-center">
+      <div class="db-align-vertical-center db-flex-item-1 db-left-text db-vertical-center-text" v-if="isBP">
+        <span class="db-fill-in">Placed 1<sup>st</sup></span>
+      </div>
+      <div class="db-align-vertical-center db-flex-item-1 db-left-text db-vertical-center-text" v-if="isBP">
+        <span class="db-fill-in">Placed 2<sup>nd</sup></span>
+      </div>
+      <div class="db-align-vertical-center db-flex-item-1 db-left-text db-vertical-center-text" v-if="isBP">
+        <span class="db-fill-in">Placed 3<sup>rd</sup></span>
+      </div>
+      <div class="db-align-vertical-center db-flex-item-1 db-left-text db-vertical-center-text" v-if="isBP">
+        <span class="db-fill-in">Placed 4<sup>th</sup></span>
+      </div>
     </div>
-    <div class="db-padding-horizontal db-flex-item db-align-vertical-center db-right-text ">
-      Total:
+    <div class="db-padding-horizontal db-flex-item-fwl db-align-vertical-center db-right-text">
+      Total Score:
     </div>
     <div class="db-fill-in db-flex-item-fwl">
     </div>
@@ -83,8 +107,6 @@
       speakersCount: Number,
       showPronouns: Boolean,
       hasReplies: Boolean,
-      lowestSpeak: Number,
-      highestSpeak: Number
     },
   })
 </script>
